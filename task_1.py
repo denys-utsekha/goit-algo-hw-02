2c0e4a00fb15cd4ddcfbd2406560fb575f6ff654
Denys Utsekha <denysutsekha@gmail.com>
2024-02-19 21:08:05 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,20 +1,27 @@
 from queue import Queue
 import uuid
+import time
 
 queue = Queue()
 
 def generate_request():
-  for i in range(5):
-    new_request = { id: uuid.uuid1() }
-    queue.put(new_request)
+  id = uuid.uuid1()
+  new_request = { id }
+  queue.put(new_request)
+  print(f"Згенеровано нову за'явку id: {id}")
+  time.sleep(1)
 
 def process_request():
   while not queue.empty():
-    request_from_queue = queue.get()  
-  print("Queue is empty!")
+    request_from_queue = queue.get()
+    print(f"Оброблено за'явку id: {request_from_queue}")
+    time.sleep(1)
+  print("Заявок немає.")
 
 try:
   while True:
+    generate_request()
+    generate_request()
     generate_request()
     process_request()
 except KeyboardInterrupt:
