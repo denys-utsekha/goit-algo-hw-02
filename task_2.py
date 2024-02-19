c6fc32ff9f19364bf20f19cbac4acecb48642d38
Denys Utsekha <denysutsekha@gmail.com>
2024-02-19 17:54:48 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -2,24 +2,13 @@ from collections import deque
 import re
 
 def is_palindrome(text: str) -> bool:
-  transformed_text = re.sub(r"[!.,\s]", "", text)
-  d = deque()
-  [d.append(char) for char in transformed_text]
+  d = deque(re.sub(r"[!.,\s]", "", text))
 
-  res = True
+  while len(d) > 1:
+    if d.popleft().lower() != d.pop().lower():
+      return False
 
-  while len(d) != 0:
-    if len(d) == 1:
-      break
-    
-    left = d.popleft()
-    right = d.pop()
-    
-    if left.lower() != right.lower():
-      res = False
-      break
-
-  return res
+  return True
 
 palindrome = input("Введіть текст: ")
 
